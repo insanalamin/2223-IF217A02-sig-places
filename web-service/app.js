e9a8880be3c414584b403a320c3b3d3b64b0fbe2
Muhammad Insan Al-Amin <muhammad.insanalamin@uinsgd.ac.id>
2022-10-11 13:02:35 +0700

    fix: GeoJSON query

--- web-service/app.js
+++ web-service/app.js
@@ -22,18 +22,37 @@ app.get('/places', (objekRequest, objekResponse) => {
     console.log("Connected !") 
   })
 
-  client.query(`SELECT
-    id_tempat,
-    nama_tempat,
-    kategori,
-    ST_AsText(koordinat) as koordinat,
-    ST_AsText(lahan) as lahan 
-  FROM 
-    jabar.pariwisata
+  // client.query(`SELECT
+  //   id_tempat,
+  //   nama_tempat,
+  //   kategori,
+  //   ST_AsText(koordinat) as koordinat,
+  //   ST_AsText(lahan) as lahan 
+  // FROM 
+  //   jabar.pariwisata
+  //   `, (erornyaDatabase, hasilDariDatabase) => {
+
+  //   objekResponse.json({
+  //     data: hasilDariDatabase.rows,
+  //   })  
+  // })
+
+  client.query(`
+    SELECT JSONB_BUILD_OBJECT(
+      'type', 'FeatureCollection',
+      'features', JSON_AGG(features.feature)
+    ) 
+    FROM (
+      SELECT row_to_json(inputs) As feature 
+         FROM (SELECT 'Feature' As type 
+         , ST_AsGeoJSON(l.lahan)::json As geometry 
+         , row_to_json((SELECT l FROM (SELECT id_tempat, nama_tempat, kategori) As l)) As properties 
+         FROM jabar.pariwisata As l WHERE l.lahan is not NULL) As inputs
+    ) features
     `, (erornyaDatabase, hasilDariDatabase) => {
 
     objekResponse.json({
-      data: hasilDariDatabase.rows,
+      data: hasilDariDatabase.rows[0],
     })  
   })
   
