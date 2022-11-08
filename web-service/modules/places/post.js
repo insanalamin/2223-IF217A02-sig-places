cbcb8e6ad2eea9aa9c2cda073e385bcd066c0618
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-11-08 14:37:34 +0700

    Update post.js

--- web-service/modules/places/post.js
+++ web-service/modules/places/post.js
@@ -3,26 +3,33 @@ const { connectionSettings } = require("../../shared/configs")
 
 const placesPOST = (objekRequest, objekResponse) => {
 
-  const client = new Client(connectionSettings)
+  // Ambil objek body dari POST nya
+  const requestBody = objekRequest.body
+  console.log("requestBody", requestBody)
 
-  const insertedData = {
-    test: "yoi"
-  }
+  const client = new Client(connectionSettings)
 
   client.connect(err => {
     if(err) throw err
     console.log("Connected !") 
   })
 
-  client.query(`
-    INSERT INTO jabar.pariwisata(nama_tempat, kategori, koordinat)
-    VALUES('Paris Van Java', 4, ST_GeomFromText('POINT(107.6191 6.9175)', 4326));
-    `, (erornyaDatabase, hasilDariDatabase) => {
-
-    objekResponse.json({
-      data: insertedData,
-    })  
-  })
+  client.query(
+    `
+      INSERT INTO jabar.pariwisata(nama_tempat, kategori, koordinat, lahan, jalur)
+      VALUES($1, $2, ST_GeomFromText($3, 4326), ST_GeomFromText($4, 4326), ST_GeomFromText($5, 4326));
+    `, 
+    [
+      requestBody['nama_tempat'],
+      requestBody['kategori'],
+      requestBody['koordinat'],
+      requestBody['lahan'],
+      requestBody['jalur'],
+    ],
+    (erornyaDatabase, hasilDariDatabase) => {
+      objekResponse.json(objekRequest.body)  
+    },
+  )
 
   
 }
